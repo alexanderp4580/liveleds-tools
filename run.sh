710b16eff207daa4a77a19714a529fe2b5bda14c
Alexander Ponomarev <alexander.ponomarev@ing.com>
2022-03-06 18:47:46 +0100

    Added default sink

--- run.sh
+++ run.sh
@@ -59,6 +59,7 @@ sleep 5
 
 pacmd load-module module-remap-source source_name=virt_ll_source_mono master=alsa_output.platform-bcm2835_audio.analog-stereo.monitor channel_map=mono channels=1
 pacmd load-module module-loopback source=alsa_output.platform-bcm2835_audio.analog-stereo.monitor sink=alsa_output.platform-bcm2835_audio.digital-stereo latency_msec=20
+pactl set-default-sink alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo
 
 sleep 5
 
